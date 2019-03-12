38da12e268e6c11025c4bb30f15378b404f14dde
Oli Nelson <olivernelson@icloud.com>
2019-03-12 17:34:17 -0400

    added basic nav and react router

--- src/App.js
+++ src/App.js
@@ -2,27 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// react router
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// components
+import NavBar from './components/NavBar'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <NavBar />
+
+          <Route exact path="/" component={this.Home} />
+          
+        </div>
+      </Router>
     );
   }
+   Home() {
+  return <h2>Home</h2>;
+}
+
+
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NavBar = props => {
+    return (
+        <nav>
+            <h4>PodSearcher</h4>
+        </nav>
+    )
+}
+
+export default NavBar
