0aa76ed6138e925b60ef645d40ccc5feb2ec5c08
olinelson <olivernelson@icloud.com>
2019-03-19 12:07:50 -0400

    redirect after post

--- src/components/AddClipForm.js
+++ src/components/AddClipForm.js
@@ -1,13 +1,14 @@
-import React, {Component, Fragment } from "react";
-import LoadingBar from "./LoadingBar"
+import React, { Component, Fragment } from "react";
+import LoadingBar from "./LoadingBar";
+
+import { withRouter } from "react-router-dom";
 
 class AddClipForm extends Component {
+  state = {
+    fileUploading: false
+  };
 
-    state = {
-        loading: false
-    }
-
-    submitHandler = e => {
+  submitHandler = e => {
     e.preventDefault();
 
     let formData = new FormData();
@@ -15,33 +16,33 @@ class AddClipForm extends Component {
     formData.append("audio_file", e.target.fileInput.files[0]);
     formData.append("name", e.target.name.value);
 
-    this.setState({loading: true})
+    this.setState({ fileUploading: true });
 
     fetch("http://localhost:3000/api/v1/clips", {
       method: "POST",
       body: formData
-    }).then( () => this.setState({loading: false}))
+    }).then(() => this.setState({ fileUploading: false }))
+    .then(() => this.props.history.push("/"))
   }; // end of submitHandler
 
-
-  render(){
-      return (
-          <Fragment>
-              <LoadingBar loading={this.state.loading}/>
-              <div className="add-clip-form">
-                  <h4>create episode</h4>
-                  <form onSubmit={this.submitHandler}>
-                      <label>Name</label>
-                      <input name="name" />
-                      <label>File Upload</label>
-                      <input name="fileInput" type="file" />
-                      <button>submit</button>
-                  </form>
-              </div>
-          </Fragment>
-      );
+  render() {
+    console.log(this.props);
+    return (
+      <Fragment>
+        <LoadingBar loading={this.state.fileUploading} />
+        <div className="add-clip-form">
+          <h4>create episode</h4>
+          <form onSubmit={this.submitHandler}>
+            <label>Name</label>
+            <input name="name" />
+            <label>File Upload</label>
+            <input name="fileInput" type="file" />
+            <button disabled={this.state.fileUploading}>submit</button>
+          </form>
+        </div>
+      </Fragment>
+    );
   }
-  
 }
 
-export default AddClipForm;
+export default withRouter(AddClipForm);
