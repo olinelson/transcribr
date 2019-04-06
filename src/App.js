27472a4e4e301b12911191f552b453edec217e3d
olinelson <olivernelson@icloud.com>
2019-04-06 12:49:04 -0400

    cleaned up App

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import "./App.css";
 // font awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-
 import { faCheckSquare, faCoffee, faCheck, faUser, faSadTear } from '@fortawesome/free-solid-svg-icons'
-
-
 
 // react router
 import { BrowserRouter as Router, Route, Link, withRouter } from "react-router-dom";
@@ -29,6 +26,7 @@ import Feed from "./hocs/Feed"
 // api URL
 import API_URL from "./components/config"
 
+// font awesome icons in use
 library.add(fab, faCheckSquare, faCoffee, faCheck, faUser, faSadTear)
 
 // filtered clips and clips used for results in clips container
@@ -39,45 +37,32 @@ class App extends Component {
 
   componentDidMount() {
     this.getCurrentUser()
-
   }
 
   // auto login method that sets current user if one has already logged in
   getCurrentUser= () => {
-
     let token = localStorage.getItem("token")
     if (token) {
-
       fetch(`${API_URL}/auto_login`, {
           method: "GET",
           headers: {
             "Authorization": token
           }
         })
-        .then(r => r.json())
-        .then(r => this.setState({
-          currentUser: r,
+      .then(r => r.json())
+      .then(r => this.setState({
+         currentUser: r,
         }))
-
-
     }
   }
 
-  
-
-
   // for use with the login form component
   setCurrentUser = (response) => {
-
-  
     this.setState({
       currentUser: response.user,
-   
     })
+
     localStorage.setItem("token", response.jwt)
-   
-
-
   }
 
 
@@ -104,13 +89,10 @@ class App extends Component {
             <Route path="/signup" component={this.SignUp} />
             <Route path="/users/:id" component={this.UserShow}/>
             <Route path="/login" render={(routerProps) => <LoginForm {...routerProps} getCurrentUser={this.getCurrentUser} setCurrentUser={this.setCurrentUser}/> } />
-
-
           </div>
         </Fragment>
-      
     );
-  }
+  } // end of render method
 
   Home = () => {
     return(
@@ -118,67 +100,62 @@ class App extends Component {
     )
   }
 
- 
-
-
-
   Feed = () => {
     return (
-      <Feed filterClips={this.filterClips}
-          getAllClips={this.getAllClips}
-          filteredClips={this.state.filteredClips}
-          clips={this.state.clips}
-          />
+      <Feed 
+        filterClips={this.filterClips}
+        getAllClips={this.getAllClips}
+        filteredClips={this.state.filteredClips}
+        clips={this.state.clips}
+      />
     );
-  }; //end of Feed
-
+  };
 
 
   ClipShow = ({ match }) => {
     // id used to find clip in clip component with its own fetch request
     let id = match.params.id
     return (
-        <Clip  
+      <Clip  
         currentUser={this.state.currentUser}
         id={id}
         getCurrentUser={this.getCurrentUser}
-        />
+      />
     );
-  };
+  }; 
+
 
   UserShow = ({match}) => {
     return (
       <User
-       currentUser={this.state.currentUser}
-       getCurrentUser={this.getCurrentUser}
+        currentUser={this.state.currentUser}
+        getCurrentUser={this.getCurrentUser}
       
-        />
+      />
     );
   };
 
+
   Upload = () => {
     return (
-      <Fragment>
         <AddClipForm  
           getCurrentUser={this.getCurrentUser} 
           getAllClips={this.getAllClips} 
           currentUser={this.state.currentUser}
           getUsersClips={this.getUsersClips}
-           />
-
-      </Fragment>
+        />
     );
   };
 
+
   SignUp = () => {
     return (
-      <Fragment>
         <SignUpForm />
-      </Fragment>
     );
   };
 
 } 
+{/* end of app */}
 
 
 
