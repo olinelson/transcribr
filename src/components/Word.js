32eab918516c51b03190062f0d4eec131f705cae
olinelson <olivernelson@icloud.com>
2019-06-02 17:52:14 -0400

    f

--- src/components/Word.js
+++ src/components/Word.js
@@ -20,33 +20,7 @@ class Word extends Component {
 
   render() {
     return (
-    //   <p
-    //     style={{
-    //       display: "inline-flex",
-          
-    //       border: "1px solid green",
-
-    //     }}
-    //   >
-
-    //       <span
-    //         onClick={this.onClickHandler}
-    //         onMouseEnter={this.showTimeStamp}
-    //         onMouseLeave={this.hideTimeStamp}
-
-    //       >
-    //         {this.props.word.word}
-    //       </span>
-
-
-    //       {this.state.hover === true ? (
-    //         <Label pointing>{this.props.word.start_time}</Label>
-    //       ) : (
-    //         <Label pointing style={{visibility: "hidden"}}>{this.props.word.start_time}</Label>
-    //       )}
-
-    //   </p>
-     <div onClick={this.onClickHandler} onMouseEnter={this.showTimeStamp} onMouseLeave={this.hideTimeStamp} className="word">
+       <div onClick={this.onClickHandler} onMouseEnter={this.showTimeStamp} onMouseLeave={this.hideTimeStamp} className="word">
         <p>{this.props.word.word}</p>
         {this.state.clicked === true ? (
           <p className="word-stamp">{this.props.word.start_time}</p>
