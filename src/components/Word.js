e42d0ae1e20cfe7189c511f698f87ab21197d83c
olinelson <olivernelson@icloud.com>
2019-06-02 16:55:21 -0400

    changed words

--- src/components/Word.js
+++ src/components/Word.js
@@ -20,33 +20,39 @@ class Word extends Component {
 
   render() {
     return (
-      <p
-        style={{
-          display: "inline-flex",
+    //   <p
+    //     style={{
+    //       display: "inline-flex",
           
-          border: "1px solid green",
+    //       border: "1px solid green",
 
-        }}
-      >
+    //     }}
+    //   >
 
-          <span
-            onClick={this.onClickHandler}
-            onMouseEnter={this.showTimeStamp}
-            onMouseLeave={this.hideTimeStamp}
+    //       <span
+    //         onClick={this.onClickHandler}
+    //         onMouseEnter={this.showTimeStamp}
+    //         onMouseLeave={this.hideTimeStamp}
 
-          >
-            {this.props.word.word}
-          </span>
+    //       >
+    //         {this.props.word.word}
+    //       </span>
 
 
-          {this.state.hover === true ? (
-            <Label pointing>{this.props.word.start_time}</Label>
-          ) : (
-            <Label pointing style={{visibility: "hidden"}}>{this.props.word.start_time}</Label>
-          )}
+    //       {this.state.hover === true ? (
+    //         <Label pointing>{this.props.word.start_time}</Label>
+    //       ) : (
+    //         <Label pointing style={{visibility: "hidden"}}>{this.props.word.start_time}</Label>
+    //       )}
 
-      </p>
-    );
+    //   </p>
+     <div onClick={this.onClickHandler} onMouseEnter={this.showTimeStamp} onMouseLeave={this.hideTimeStamp} className="word">
+        <p>{this.props.word.word}</p>
+        {this.state.clicked === true ? (
+          <p className="word-stamp">{this.props.word.start_time}</p>
+        ) : null}
+      </div>
+    )
   }
 }
 
